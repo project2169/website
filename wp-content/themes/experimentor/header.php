fb8639abc3f4c74fff84ab365c06aa61f2d672b6
Smit2808 <64188895+Smit2808@users.noreply.github.com>
2021-07-31 18:33:38 +0530

    static header

---
+++ wp-content/themes/experimentor/header.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package experimentor
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="<?php bloginfo('charset');?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <header class="site-header">
+            <div class="container">
+                <div class="site_header_content">
+                    <div class = "site_logo">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.jpg" alt="">
+					</div>
+                    <nav class="experimentor_navbar">
+                        <ul>
+							<li><a href="">About</a></li>
+							<li><a href="">Mentor</a></li>
+							<li><a href="">Course</a></li>
+							<li><a href="">R&D</a></li>
+							<li><a href="">Internship</a></li>
+							<li><a href="">Research</a></li>
+							<li><a href="">Contact Us</a></li>
+						</ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
